e3abd85675e33e8961273d425eca90cecb93da98
e-lulu <lucie.vigreux@laposte.net>
2015-12-04 19:24:09 +0100

    micro commit

--- src/run/Main.java
+++ src/run/Main.java
@@ -10,8 +10,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		final PluginFinder finder = new PluginFinder("dropins");
-		PluginObserver observer = new PluginLogger();
-		finder.addObserver(observer);
+		PluginObserver logger = new PluginLogger();
+		finder.addObserver(logger);
 		new Timer(1000, finder).start();
 		while (true);
 	}
