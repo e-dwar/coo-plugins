c97353cc678ab06794aabb3e23a213da492927fb
e-dwar <mailfork-github2@yahoo.com>
2015-12-05 21:36:01 +0100

    corrections sur PluginLoader

--- src/plugins/PluginLoader.java
+++ src/plugins/PluginLoader.java
@@ -3,6 +3,8 @@ package plugins;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+
 import exception.PluginLoadingException;
 
 public class PluginLoader extends ClassLoader {
@@ -11,20 +13,28 @@ public class PluginLoader extends ClassLoader {
 		super(PluginLoader.class.getClassLoader());
 	}
 
+	// see http://stackoverflow.com/a/3971771/1636522
 	public Plugin loadPlugin(File file) throws PluginLoadingException {
 		Class<?> cls = null;
 		String clsName = file.getName();
 		int i = clsName.lastIndexOf(".");
-		clsName = clsName.substring(0, i);
 		int size = (int) file.length();
 		byte bytes[] = new byte[size];
+		clsName = clsName.substring(0, i);
 		try {
 			FileInputStream fis = new FileInputStream(file);
 			DataInputStream dis = new DataInputStream(fis);
 			dis.readFully(bytes);
 			dis.close();
 			cls = defineClass(clsName, bytes, 0, bytes.length);
-			cls = super.loadClass(clsName);
+		} catch (IOException ioe) {
+			try {
+				cls = super.loadClass(clsName);
+			} catch (ClassNotFoundException e) {
+				throw new PluginLoadingException(e.getMessage());
+			}
+		}
+		try {
 			return (Plugin) cls.newInstance();
 		} catch (Exception e) {
 			throw new PluginLoadingException(e.getMessage());
